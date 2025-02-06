ec24d994f5e427836f8a59e7615cbebb81c3bcef
blacklungLL <145188803+blacklungLL@users.noreply.github.com>
2025-02-06 17:28:12 +0300

    Changed design for game form

--- AliasClient/GameForm.Designer.cs
+++ AliasClient/GameForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace AliasClient
+namespace AliasClient
 {
     partial class GameForm
     {
@@ -40,112 +40,141 @@
             this.ChatTextBox = new System.Windows.Forms.TextBox();
             this.ChatInputTextBox = new System.Windows.Forms.TextBox();
             this.SendMessageButton = new System.Windows.Forms.Button();
-
             this.SuspendLayout();
-            // 
+
+            // General form settings
+            this.BackColor = System.Drawing.Color.FromArgb(30, 30, 30); // Dark background
+            this.ForeColor = System.Drawing.Color.White; // Light text
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+
             // CurrentTeamLabel
-            // 
             this.CurrentTeamLabel.AutoSize = true;
+            this.CurrentTeamLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.CurrentTeamLabel.Location = new System.Drawing.Point(20, 20);
             this.CurrentTeamLabel.Name = "CurrentTeamLabel";
-            this.CurrentTeamLabel.Size = new System.Drawing.Size(93, 17);
+            this.CurrentTeamLabel.Size = new System.Drawing.Size(120, 28);
             this.CurrentTeamLabel.TabIndex = 0;
             this.CurrentTeamLabel.Text = "Current Team: ";
-            // 
+
             // WordLabel
-            // 
-            this.WordLabel.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.WordLabel.Location = new System.Drawing.Point(20, 50);
+            this.WordLabel.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.WordLabel.ForeColor = System.Drawing.Color.FromArgb(0, 191, 255); // Bright blue word color
+            this.WordLabel.Location = new System.Drawing.Point(20, 60);
             this.WordLabel.Name = "WordLabel";
-            this.WordLabel.Size = new System.Drawing.Size(300, 50);
+            this.WordLabel.Size = new System.Drawing.Size(760, 50);
             this.WordLabel.TabIndex = 1;
             this.WordLabel.Text = "Word: ???";
             this.WordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
+
             // TimerLabel
-            // 
             this.TimerLabel.AutoSize = true;
-            this.TimerLabel.Location = new System.Drawing.Point(20, 110);
+            this.TimerLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TimerLabel.Location = new System.Drawing.Point(20, 120);
             this.TimerLabel.Name = "TimerLabel";
-            this.TimerLabel.Size = new System.Drawing.Size(86, 17);
+            this.TimerLabel.Size = new System.Drawing.Size(100, 28);
             this.TimerLabel.TabIndex = 2;
-            this.TimerLabel.Text = "Time left: 3:00";
-            // 
+            this.TimerLabel.Text = "Time left: 1:00";
+
             // Team1ScoreLabel
-            // 
             this.Team1ScoreLabel.AutoSize = true;
-            this.Team1ScoreLabel.Location = new System.Drawing.Point(20, 140);
+            this.Team1ScoreLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Team1ScoreLabel.Location = new System.Drawing.Point(20, 160);
             this.Team1ScoreLabel.Name = "Team1ScoreLabel";
-            this.Team1ScoreLabel.Size = new System.Drawing.Size(76, 17);
+            this.Team1ScoreLabel.Size = new System.Drawing.Size(90, 28);
             this.Team1ScoreLabel.TabIndex = 3;
             this.Team1ScoreLabel.Text = "Team 1: 0";
-            // 
+
             // Team2ScoreLabel
-            // 
             this.Team2ScoreLabel.AutoSize = true;
-            this.Team2ScoreLabel.Location = new System.Drawing.Point(20, 160);
+            this.Team2ScoreLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Team2ScoreLabel.Location = new System.Drawing.Point(20, 190);
             this.Team2ScoreLabel.Name = "Team2ScoreLabel";
-            this.Team2ScoreLabel.Size = new System.Drawing.Size(76, 17);
+            this.Team2ScoreLabel.Size = new System.Drawing.Size(90, 28);
             this.Team2ScoreLabel.TabIndex = 4;
             this.Team2ScoreLabel.Text = "Team 2: 0";
-            // 
+
             // CurrentPlayerLabel
-            // 
             this.CurrentPlayerLabel.AutoSize = true;
-            this.CurrentPlayerLabel.Location = new System.Drawing.Point(20, 190);
+            this.CurrentPlayerLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CurrentPlayerLabel.Location = new System.Drawing.Point(20, 220);
             this.CurrentPlayerLabel.Name = "CurrentPlayerLabel";
-            this.CurrentPlayerLabel.Size = new System.Drawing.Size(106, 17);
+            this.CurrentPlayerLabel.Size = new System.Drawing.Size(140, 28);
             this.CurrentPlayerLabel.TabIndex = 5;
             this.CurrentPlayerLabel.Text = "Current Player: ";
-            // 
-            // Buttons
-            // 
-            this.GuessButton.Location = new System.Drawing.Point(20, 220);
+
+            // GuessButton
+            this.GuessButton.Location = new System.Drawing.Point(20, 270);
             this.GuessButton.Name = "GuessButton";
-            this.GuessButton.Size = new System.Drawing.Size(100, 30);
+            this.GuessButton.Size = new System.Drawing.Size(150, 40);
+            this.GuessButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.GuessButton.BackColor = System.Drawing.Color.FromArgb(76, 175, 80); // Dark green
+            this.GuessButton.ForeColor = System.Drawing.Color.White;
+            this.GuessButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat; // Flat style
+            this.GuessButton.FlatAppearance.BorderSize = 0; // Remove border
             this.GuessButton.TabIndex = 6;
             this.GuessButton.Text = "Guess";
-            this.GuessButton.UseVisualStyleBackColor = true;
+            this.GuessButton.UseVisualStyleBackColor = false;
             this.GuessButton.Click += new System.EventHandler(this.GuessButton_Click);
 
-            this.SkipButton.Location = new System.Drawing.Point(130, 220);
+            // SkipButton
+            this.SkipButton.Location = new System.Drawing.Point(180, 270);
             this.SkipButton.Name = "SkipButton";
-            this.SkipButton.Size = new System.Drawing.Size(100, 30);
+            this.SkipButton.Size = new System.Drawing.Size(150, 40);
+            this.SkipButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.SkipButton.BackColor = System.Drawing.Color.FromArgb(158, 158, 158); // Gray
+            this.SkipButton.ForeColor = System.Drawing.Color.Black;
+            this.SkipButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat; // Flat style
+            this.SkipButton.FlatAppearance.BorderSize = 0; // Remove border
             this.SkipButton.TabIndex = 7;
             this.SkipButton.Text = "Skip";
-            this.SkipButton.UseVisualStyleBackColor = true;
+            this.SkipButton.UseVisualStyleBackColor = false;
             this.SkipButton.Click += new System.EventHandler(this.SkipButton_Click);
 
-            this.SurrenderButton.Location = new System.Drawing.Point(240, 220);
+            // SurrenderButton
+            this.SurrenderButton.Location = new System.Drawing.Point(340, 270);
             this.SurrenderButton.Name = "SurrenderButton";
-            this.SurrenderButton.Size = new System.Drawing.Size(100, 30);
+            this.SurrenderButton.Size = new System.Drawing.Size(150, 40);
+            this.SurrenderButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.SurrenderButton.BackColor = System.Drawing.Color.FromArgb(211, 47, 47); // Dark red
+            this.SurrenderButton.ForeColor = System.Drawing.Color.White;
+            this.SurrenderButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat; // Flat style
+            this.SurrenderButton.FlatAppearance.BorderSize = 0; // Remove border
             this.SurrenderButton.TabIndex = 8;
             this.SurrenderButton.Text = "Surrender";
-            this.SurrenderButton.UseVisualStyleBackColor = true;
+            this.SurrenderButton.UseVisualStyleBackColor = false;
             this.SurrenderButton.Click += new System.EventHandler(this.SurrenderButton_Click);
-            // 
+
             // Chat TextBox
-            // 
-            this.ChatTextBox.Location = new System.Drawing.Point(20, 270);
+            this.ChatTextBox.Location = new System.Drawing.Point(20, 330);
             this.ChatTextBox.Multiline = true;
             this.ChatTextBox.ReadOnly = true;
             this.ChatTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.ChatTextBox.Size = new System.Drawing.Size(760, 250);
+            this.ChatTextBox.Size = new System.Drawing.Size(760, 200);
+            this.ChatTextBox.BackColor = System.Drawing.Color.FromArgb(60, 60, 60);
+            this.ChatTextBox.ForeColor = System.Drawing.Color.White;
+            this.ChatTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.ChatTextBox.TabIndex = 9;
 
             // Chat Input TextBox
-            this.ChatInputTextBox.Location = new System.Drawing.Point(20, 530);
+            this.ChatInputTextBox.Location = new System.Drawing.Point(20, 540);
             this.ChatInputTextBox.Name = "ChatInputTextBox";
-            this.ChatInputTextBox.Size = new System.Drawing.Size(600, 22);
+            this.ChatInputTextBox.Size = new System.Drawing.Size(600, 30);
+            this.ChatInputTextBox.BackColor = System.Drawing.Color.FromArgb(60, 60, 60);
+            this.ChatInputTextBox.ForeColor = System.Drawing.Color.White;
+            this.ChatInputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.ChatInputTextBox.TabIndex = 10;
 
             // Send Chat Message Button
-            this.SendMessageButton.Location = new System.Drawing.Point(630, 530);
+            this.SendMessageButton.Location = new System.Drawing.Point(630, 540);
             this.SendMessageButton.Name = "SendMessageButton";
             this.SendMessageButton.Size = new System.Drawing.Size(150, 30);
+            this.SendMessageButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.SendMessageButton.BackColor = System.Drawing.Color.FromArgb(0, 122, 204); // Blue accent
+            this.SendMessageButton.ForeColor = System.Drawing.Color.White;
+            this.SendMessageButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SendMessageButton.TabIndex = 11;
             this.SendMessageButton.Text = "Send";
-            this.SendMessageButton.UseVisualStyleBackColor = true;
+            this.SendMessageButton.UseVisualStyleBackColor = false;
             this.SendMessageButton.Click += new System.EventHandler(this.SendMessageButton_Click);
 
             // Form settings
